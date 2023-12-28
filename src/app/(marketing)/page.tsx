44cb985e55024b604d37d903f9adb3ab6312d129
bahadirk5 <korkmazerb0@gmail.com>
2023-12-28 14:43:27 +0300

    .

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,11 +2,17 @@ import { getServerAuthSession } from "@/lib/auth"
 
 export default async function Home() {
   const session = await getServerAuthSession()
+
+  if (session) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <pre>{JSON.stringify(session.user, null, 2)}</pre>
+      </main>
+    )
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <pre>
-        {JSON.stringify(session?.user, null, 2)}
-      </pre>
+      hi
     </main>
   )
 }
